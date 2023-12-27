fa827be81f2d8b76f1732fb4e1a3576e4d3ec01d
Lucas Anderson <lucas.ferreira24241@alunos.ufersa.edu.br>
2023-12-27 17:32:52 -0300

    recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request,'recipes/home.html')
 
 def contato(request):
     return HttpResponse('CONTATO')
