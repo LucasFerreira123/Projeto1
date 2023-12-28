75c3eba60cb6eb6c9d3788c6ea69e7080eecf897
Lucas Anderson <lucas.ferreira24241@alunos.ufersa.edu.br>
2023-12-28 10:44:55 -0300

    Remivido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request,'recipes/home.html')
 
-def contato(request):
-    return HttpResponse('CONTATO')
 
-def sobre(request):
-    return HttpResponse('SOBRE')
